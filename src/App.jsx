b6222bc297d1fa3e8fdbb0d8557b2d018312c469
catgame345 <eduardoadlercruz@gmail.com>
2023-12-28 14:06:44 -0600

    cambio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react"
-
 function App() {
-  const [counter, useCounter] = useState(0)
   return (
     <>
-      <div className={`text-center border`}>
-        <button onClick={() => useCounter(counter + 1)} className={`text-center border border-red-500 bg-red-500`}>
-          Counter: {counter}
-        </button>
+      <div>
+        <h1 className="text-center text-4xl">
+          Bienvenido o bienvenida a esta pagina web
+        </h1>
       </div>
     </>
   )
